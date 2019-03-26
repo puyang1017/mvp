615de0c167c6d05040043e3db197bd3e9850eea3
puyang1017 <puyang1017@gmail.com>
2019-03-26 17:04:11 +0800

    修改业务码

--- puymvpjava/src/main/java/com/android/puy/puymvpjava/net/NetError.java
+++ puymvpjava/src/main/java/com/android/puy/puymvpjava/net/NetError.java
@@ -4,15 +4,15 @@ package com.android.puy.puymvpjava.net;
 public class NetError extends Exception {
     private Throwable exception;
     private int type = NoConnectError;
-    private int httpType;
+    private int httpType = 200;
 
-    public static final int ParseError = 650;   //数据解析异常
-    public static final int NoConnectError = 651;   //无连接异常
-    public static final int AuthError = 652;   //用户验证异常
-    public static final int NoDataError = 653;   //无数据返回异常
-    public static final int BusinessError = 654;   //业务异常
-    public static final int OtherError = 655;   //其他异常
-    public static final int HttpError = 656;   //Http异常
+    public static final int ParseError = 2650;   //数据解析异常
+    public static final int NoConnectError = 2651;   //无连接异常
+    public static final int AuthError = 2652;   //用户验证异常
+    public static final int NoDataError = 2653;   //无数据返回异常
+    public static final int BusinessError = 2654;   //业务异常
+    public static final int OtherError = 2655;   //其他异常
+    public static final int HttpError = 2656;   //Http异常
 
     public NetError(Throwable exception, int type, int httpType) {
         this.exception = exception;
