dbc04af32aafeb078273e1195befa6169316bb15
puy <>
2019-04-18 23:33:02 +0800

    上传

--- app/src/main/java/com/android/puy/mvpkotlin/MainActivity.kt
+++ app/src/main/java/com/android/puy/mvpkotlin/MainActivity.kt
@@ -2,7 +2,8 @@ package com.android.puy.mvpkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import coroutines.KotlinAsyncWithCoroutines.Companion.loadAsync
+import coroutines.KotlinAsyncWithCoroutines.Companion.ui
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
