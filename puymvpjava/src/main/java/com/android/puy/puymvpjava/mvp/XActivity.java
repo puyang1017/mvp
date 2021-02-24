d20a78555a40dd088e9aa94d790b22f81c12b436
puyang1017 <puyang1017@gmail.com>
2021-02-24 13:54:04 +0800

    添加导航栏颜色设置

--- puymvpjava/src/main/java/com/android/puy/puymvpjava/mvp/XActivity.java
+++ puymvpjava/src/main/java/com/android/puy/puymvpjava/mvp/XActivity.java
@@ -160,6 +160,15 @@ public abstract class XActivity<P extends IPresent> extends RxAppCompatActivity 
         mImmersionBar.init();
     }
 
+    public void initStatusBar(int id, boolean statusBarDark, int navigationBarColor) {
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.titleBar(id);
+        mImmersionBar.statusBarDarkFont(statusBarDark);
+        mImmersionBar.keyboardEnable(true);
+        mImmersionBar.navigationBarColor(navigationBarColor);
+        mImmersionBar.init();
+    }
+
     public void setMaterialRipple(int color,View ...views){
         for (View view:views){
             MaterialRippleLayout.on(view)
